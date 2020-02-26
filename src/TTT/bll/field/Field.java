6ab88602955f0c2d197bf5a901f30ec0a6a9fbd7
SnowBoy73 <alan4999@gmail.com>
2020-02-26 15:53:14 +0100

    Update Field.java

--- src/TTT/bll/field/Field.java
+++ src/TTT/bll/field/Field.java
@@ -23,34 +23,32 @@ public class Field implements IField {
     
     @Override
     public void clearBoard() {
-        
         for (int microX = 0; microX < microboard.length; microX ++) { 
             for (int microY = 0; microY < microboard.length; microY ++) { 
                 microboard [microX][microY] = EMPTY_FIELD;
 //System.out.println(microX + " " + microY + "=" + EMPTY_FIELD);
             }
         }
-        
         for (int macroX = 0; macroX < macroboard.length; macroX ++) { 
             for (int macroY = 0; macroY < macroboard.length; macroY ++) { 
                 macroboard [macroX][macroY] = AVAILABLE_FIELD;
 //System.out.println(macroX + " " + macroY + "=" + AVAILABLE_FIELD);
             }
         }
-        
     }
 
     
     @Override
     public List<IMove> getAvailableMoves() {
         List<IMove> availableMoves = new ArrayList<>();
-
+   //     IMove possibleMove = new IMove();
+          
         for (int macroX = 0; macroX < macroboard.length; macroX ++) { 
             for (int macroY = 0; macroY < macroboard.length; macroY ++) { 
-                if (macroboard [macroX][macroY] == AVAILABLE_FIELD) {
+                if ((macroboard [macroX][macroY]).equals(AVAILABLE_FIELD)) {
                     for (int microX = (macroX*3); microX < ((microboard.length)-((2-macroX)*3)); microX ++) { 
                         for (int microY = (macroX*3); microY < ((microboard.length)-((2-macroY)*3)); microY ++) { 
-                            if(microboard[microX][microY] == EMPTY_FIELD) {
+                            if((microboard[microX][microY]).equals(EMPTY_FIELD)) {
                //                 availableMoves.add([microX][microY])  // Whatever an IMove is.
                             }
                         }        
