674e615251f8b9bd65781aff1832b1619cc05cc5
filipus30 <56824843+filipus30@users.noreply.github.com>
2020-02-27 01:42:57 +0100

    Create GameState.java

---
+++ src/TTT/bll/game/GameState.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TTT.bll.game;
+
+import TTT.bll.field.IField;
+
+/**
+ *
+ * @author macos
+ */
+public class GameState implements IGameState{
+    
+    IField field;
+    int moveNumber;
+    int roundNumber;
+
+    public GameState()
+    {
+        //field = new Field();
+        moveNumber=0;
+        roundNumber=0;
+    }
+    
+    public GameState(IGameState state) {
+       // field = new Field();
+        field.setMacroboard(state.getField().getMacroboard());
+        field.setBoard(state.getField().getBoard());
+    }
+
+    @Override
+    public IField getField() {
+        return field;
+    }
+
+    @Override
+    public int getMoveNumber() {
+        return moveNumber;
+    }
+
+    @Override
+    public void setMoveNumber(int moveNumber) {
+        this.moveNumber = moveNumber;
+    }
+
+    @Override
+    public int getRoundNumber() {
+        return roundNumber;
+    }
+
+    @Override
+    public void setRoundNumber(int roundNumber) {
+        this.roundNumber = roundNumber;
+    }
+}
+
+ 
+
