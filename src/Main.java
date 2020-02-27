9a9f39398ef59ca9cd9d57bb608900b904c2ff78
TriggerStorm <michael.h.p@hotmail.com>
2020-02-27 16:24:17 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("TTT/gui/view/mok.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("TTT/gui/view/GameBord.fxml"));
         
         Scene scene = new Scene(root);
         
