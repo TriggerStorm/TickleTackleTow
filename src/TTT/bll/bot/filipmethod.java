100680824eb6e2711f8c6c64559c8449259f3b36
filipus30 <56824843+filipus30@users.noreply.github.com>
2020-03-04 11:35:53 +0100

    Update filipmethod.java

--- src/TTT/bll/bot/filipmethod.java
+++ src/TTT/bll/bot/filipmethod.java
@@ -22,11 +22,97 @@ public class filipmethod {
             {0, 0}, {2, 2}, {0, 2}, {2, 0}, 
             {0, 1}, {2, 1}, {1, 0}, {1, 2}};
    private Field f;
+   private IGameState currentState;
+   private String playerOneIcon = "O";
+    private String playerTwoIcon = "X";
+   public filipmethod()
+   {
+       this.currentState = currentState;
+   }
    public List<IMove> getMoves()
    {
        return f.getAvailableMoves();
    }
-    public IMove doMove(IGameState state) {
+   
+   public IMove moveWithWinPriority()
+   {
+       for(int i=0; i < getMoves().size();i++)
+       {
+           String [][] boardToCheck = currentState.getField().getBoard();
+           boardToCheck[getMoves().get(i).getX()][getMoves().get(i).getY()] = playerOneIcon;
+           if(checkMicroboardWin(boardToCheck,getMoves().get(i).getX(),getMoves().get(i).getY()) || checkMacroboardWin(boardToCheck))
+               return getMoves().get(i);
+           
+           boardToCheck[getMoves().get(i).getX()][getMoves().get(i).getY()] = playerTwoIcon;
+           if(checkMicroboardWin(boardToCheck,getMoves().get(i).getX(),getMoves().get(i).getY()) || checkMacroboardWin(boardToCheck))
+               return getMoves().get(i);
+           
+           
+       }
+       return findMove(currentState);
+   }
+   private boolean checkMicroboardWin(String[][] board, int posx, int posy) {   
+       
+       
+      if(checkForBoardWin(board, posx, posy))
+        return true;
+      else return false;
+    }
+    
+    private boolean checkMacroboardWin(String[][] board) {   
+       
+      if(checkForBoardWin(board,0, 0))
+        return true;
+      else return false;
+    }
+   
+    private boolean checkForBoardWin (String[][] board,int posx,int posy) 
+     {
+         for(int x = posx; x < posx+3; x++)
+        {
+            if(checkForWin(board, x, posy))
+            {
+                return true;
+            }
+            for(int y = posy; y < posy+3; y++)
+            {
+                
+                if(checkForWin(board, posx, y))
+                {
+                    return true;
+                }
+            }
+        }
+        if(checkForWin(board,posx,posy)) 
+            return true;
+        else return false;
+     }
+   
+   private boolean checkForWin (String[][] board,int posx,int posy) 
+    {   
+      if ((board[posx][posy].equals(playerOneIcon) || board[posx][posy].equals(playerTwoIcon))
+                    && board[posx][posy].equals(board[posx][posy+1]) 
+                    && board[posx][posy+1].equals(board[posx][posy+2]))
+        return true;
+      else if ((board[posx][posy].equals(playerOneIcon) || board[posx][posy].equals(playerTwoIcon))
+                    && board[posx][posy].equals(board[posx+1][posy]) 
+                    && board[posx+1][posy].equals(board[posx][posy+2]))
+          return true;
+      else if ((board[posx][posy].equals(playerOneIcon) || board[posx][posy].equals(playerTwoIcon))
+                    && board[posx][posy].equals(board[posx+1][posy+1]) 
+                    && board[posx+1][posy+1].equals(board[posx+2][posy+2]))
+          return true;
+      else if ((board[posx][posy+2].equals(playerOneIcon) || board[posx][posy+2].equals(playerTwoIcon))
+                && board[posx][posy+2].equals(board[posx+1][posy+1])
+                && board[posx+1][posy+1].equals(board[posx+2][posy]))
+          return true;
+      else
+          return false;
+    }
+   
+   
+   
+    public IMove findMove(IGameState state) {
 
         //Find macroboard to play in
         for (int[] move : preferredMoves)
@@ -46,7 +132,7 @@ public class filipmethod {
             }
         }
 
-        //NOTE: Something failed, just take the first available move I guess!
+        //NOTE: Something failed, just take the first available move
         return f.getAvailableMoves().get(0);
     }
     
