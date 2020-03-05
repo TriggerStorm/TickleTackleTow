33eff690c4f6aca7ac9705443bf3c1027b97c519
TriggerStorm <michael.h.p@hotmail.com>
2020-03-05 22:19:42 +0100

    Update Move.java

--- src/TTT/bll/move/Move.java
+++ src/TTT/bll/move/Move.java
@@ -25,4 +25,15 @@ public class Move implements IMove{
     {
         return y;
     }
+    public void setY(int y){
+        this.y=y;
+    }
+
+    public void setX(int x){
+        this.x=x;
+        
+    }
+    public String toString() {
+        return "("+x+","+y+")";
+    }
 }
